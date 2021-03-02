7b0c344b658f7747d06e3e90d06a91be21eb03c1
Maria Cassandra <73235842+MariaCassandra@users.noreply.github.com>
2021-03-02 15:47:43 +0100

    Update main.js

--- nivel_1/main.js
+++ nivel_1/main.js
@@ -1,11 +1,15 @@
  //Ejercicio 1
-var myName = ["M", "A", "R", "I", "A"];
-var vocals = ["A", "E", "I", "O", "U"];
-var numbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+var letras=["M", "A", "R", "I", "A"];
 
-for (let i = 0; i < myName.length; i++) {
-    console.log(myName[i])
-};
+var contador=0;
+
+while(contador<letras.length) {
+
+    console.log(letras[contador]);
+
+    contador++;
+
+}
 
 //Ejercicio 2
   
