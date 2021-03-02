25547284ee7b7388d9a4f9295cefd11071dbf589
Maria Cassandra <73235842+MariaCassandra@users.noreply.github.com>
2021-03-02 12:22:22 +0100

    Update main.js

--- nivel_1/main.js
+++ nivel_1/main.js
@@ -1,1 +1,23 @@
+ //Ejercicio 1
+var letras=["M", "A", "R", "I", "A"];
+
+var contador=0;
+
+while(contador<letras.length) {
+
+    console.log(letras[contador] + "<br>");
+
+    contador++;
+
+}
+
+//Ejercicio 2
+  
+  
+//Ejercicio 3
+  
+  
+//Ejercicio 4
+  
+
 
